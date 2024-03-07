84c253f905f39278b25eae2940429f47548b7237
Dimishack <dimishak.git@mail.ru>
2024-03-07 12:57:36 +0300

    Подключение модель-представление к окну

--- BookObserver/ViewModels/MainWindowViewModel.cs
+++ BookObserver/ViewModels/MainWindowViewModel.cs
@@ -4,5 +4,15 @@ namespace BookObserver.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
-    }
+		#region Title : string? - Заголовок окна
+
+		///<summary>Заголовок окна</summary>
+		private string? _title = "Проверка работы ViewModel. Проверка прошла успшено!";
+
+		///<summary>Заголовок окна</summary>
+		public string? Title { get => _title; set => Set(ref _title, value); }
+
+		#endregion
+
+	}
 }
