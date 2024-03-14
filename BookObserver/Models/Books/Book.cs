0b3a3780021255df6c6cb4062bd51fa6a88b3f7b
Dimishack <dimishak.git@mail.ru>
2024-03-14 16:50:38 +0300

    Система быстрого поиска

--- BookObserver/Models/Books/Book.cs
+++ BookObserver/Models/Books/Book.cs
@@ -8,12 +8,12 @@ namespace BookObserver.Models.Books
         public bool Stock { get; set; }
         public int? Pages { get; set; }
         public int? YearPublish { get; set; }
-        public string? Author { get; set; }
-        public string? BBK {  get; set; }
-        public string? CodeAuthor { get; set; }
-        public string? ISBN { get; set; }
-        public string? Publish { get; set; }
-        public string? Name { get; set; }
+        public string Author { get; set; } = string.Empty;
+        public string BBK {  get; set; } = string.Empty;
+        public string CodeAuthor { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public string Publish { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public Reader? Reader { get; set; }
     }
 }
--- BookObserver/ViewModels/BooksUserControlViewModel.cs
+++ BookObserver/ViewModels/BooksUserControlViewModel.cs
@@ -1,32 +1,61 @@
 ﻿using BookObserver.Models.Books;
 using BookObserver.ViewModels.Base;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 
 namespace BookObserver.ViewModels
 {
     class BooksUserControlViewModel : ViewModel
     {
-        public ObservableCollection<Book> Books { get; }
+        #region Books : ObservableCollection<Book> - Список книг
 
-        public IList<int?> ListPages
+        ///<summary>Список книг</summary>
+        private ObservableCollection<Book>? _books;
+
+        ///<summary>Список книг</summary>
+        public ObservableCollection<Book>? Books
         {
-            get
+            get => _books;
+            set
             {
-                List<int?> list = [];
-                foreach (var book in Books)
-                    if(!list.Contains(book.Pages))
-                        list.Add(book.Pages);
-                list.Sort();
-                return list;
+                if (!Set(ref _books, value)) return;
+
+                _booksView.Source = value;
+                OnPropertyChanged(nameof(BooksView));
             }
         }
 
+        #endregion
+
+        private readonly CollectionViewSource _booksView = new();
+        public ICollectionView BooksView => _booksView.View;
+
+        #region BooksFilterText : string? - фильтр книг
+
+        ///<summary>фильтр книг</summary>
+        private string? _booksFilterText;
+
+        ///<summary>фильтр книг</summary>
+        public string? BooksFilterText
+        {
+            get => _booksFilterText;
+            set
+            {
+                if(!Set(ref _booksFilterText, value)) return;
+
+                _booksView.View.Refresh();
+            }
+        }
+
+        #endregion
+
+
         public BooksUserControlViewModel()
         {
-            Books = new (Enumerable.Range(0, 10000).Select(p => new Book
+            Books = new(Enumerable.Range(0, 100000).Select(p => new Book
             {
                 Id = p,
                 BBK = $"{p}{p}",
@@ -38,6 +67,25 @@ namespace BookObserver.ViewModels
                     FirstName = "Амплитуда"
                 }
             }));
+            _booksView.Filter += BooksView_Filter;
+        }
+
+        private void BooksView_Filter(object sender, FilterEventArgs e)
+        {
+            if(e.Item is not Book book)
+            {
+                e.Accepted = false;
+                return;
+            }
+
+            var filter_text = _booksFilterText;
+            if (string.IsNullOrWhiteSpace(filter_text)) return;
+
+            if (book.BBK.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (book.Author.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (book.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
         }
     }
 }
