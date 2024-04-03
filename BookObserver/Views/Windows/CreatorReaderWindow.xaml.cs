3e862eb8287ea78c498b6bcbee3394fa7c380880
Dimishack <dimishak.git@mail.ru>
2024-04-03 19:00:28 +0300

    Событие нажатия клавиши

--- BookObserver/Views/Windows/CreatorReaderWindow.xaml.cs
+++ BookObserver/Views/Windows/CreatorReaderWindow.xaml.cs
@@ -1,9 +1,22 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace BookObserver.Views.Windows
 {
     public partial class CreatorReaderWindow : Window
     {
         public CreatorReaderWindow() => InitializeComponent();
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if((e.Key < Key.D0 || e.Key > Key.D9)
+            && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+            && e.Key != Key.OemPlus
+            && e.Key != Key.OemMinus
+            && e.Key != Key.Subtract
+            && e.Key != Key.Add
+            )
+                e.Handled = true;
+        }
     }
 }
