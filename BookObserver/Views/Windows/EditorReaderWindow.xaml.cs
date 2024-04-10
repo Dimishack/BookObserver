8c0576879dbb38850ff9bd8ade10818b2b0e39c3
Dimishack <dimishak.git@mail.ru>
2024-04-10 21:24:22 +0300

    Добавление события

--- BookObserver/Views/Windows/EditorReaderWindow.xaml.cs
+++ BookObserver/Views/Windows/EditorReaderWindow.xaml.cs
@@ -1,9 +1,22 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace BookObserver.Views.Windows
 {
     public partial class EditorReaderWindow : Window
     {
         public EditorReaderWindow() => InitializeComponent();
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key < Key.D0 || e.Key > Key.D9)
+            && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+            && e.Key != Key.OemPlus
+            && e.Key != Key.OemMinus
+            && e.Key != Key.Subtract
+            && e.Key != Key.Add
+            )
+                e.Handled = true;
+        }
     }
 }
